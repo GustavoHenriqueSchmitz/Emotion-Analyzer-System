08494588078ac23f239aff3b72aa9b67e66b1060
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2025-03-29 17:01:23 -0300

    organizing and preparing folder structure for training process

--- database-analysis.py
+++ database-analysis.py
@@ -1,14 +1,40 @@
 import pandas as pd
 
-# Read the CSV
-df = pd.read_csv("./sentiment_analysis_dataset_filtered.csv")
-
-# Count how many times each emotion appears
+# Count all emotions in the dataset
+df = pd.read_csv("./data/sentiment_analysis.csv")
 emotion_counts = df['emotion'].value_counts()
-
-# Print the full count for each unique emotion
 print(emotion_counts)
 
-# If you want just one specific emotion, for example, "happiness":
-specific_count = (df['emotion'] == 'happiness').sum()
-print("Number of rows labeled 'happiness':", specific_count)
+# Join CSV files into one
+csv_files = [
+    "table1.csv",
+    "table2.csv",
+    "table3.csv",
+    "table4.csv",
+    "table5.csv"
+]
+
+dataframes = []
+
+for file in csv_files:
+    df = pd.read_csv(file, header=None)
+    df = df.iloc[:, [0, 1]]
+    df.columns = ['text', 'emotion']
+    dataframes.append(df)
+
+combined_df = pd.concat(dataframes, ignore_index=True)
+combined_df.to_csv("sentiment_analysis.csv", index=False)
+
+# Delete all emotions keeping only a specified amount
+df = pd.read_csv("combined.csv")
+
+emotion_df = df[df["emotion"] == "emotion"]
+other_df = df[df["emotion"] != "emotion"]
+
+if len(emotion_df) > 4000:
+    emotion_df = emotion_df.sample(n=4000, random_state=42)
+else:
+    pass
+
+final_df = pd.concat([other_df, emotion_df], ignore_index=True)
+final_df.to_csv("sentiment_analysis.csv", index=False)
